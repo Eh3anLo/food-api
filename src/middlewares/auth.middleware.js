fccde988362292205fc3eb0323b96d01cf1a05e8
Ehsan <ehsunlotfy@gmail.com>
2026-06-23 17:29:54 +0330

    feat: add jwt auth middleware to authenticate user

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+
+async function authenticate(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        status: "error",
+        message: "باید وارد شوید",
+      });
+    }
+
+    const token = authHeader.split(" ")[1]; // Authorization: Bearer <token>
+    
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({
+        "status" : "error",
+        "message":"Invalid or expired token, login again"
+    })
+  }
+}
+
+
+module.exports = { authenticate }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express");
-const authRoutes = require('./routes/auth.route')
+const authRoutes = require('./routes/auth.route');
+const { authenticate } = require("./middlewares/auth.middleware");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,13 @@ app.use("/auth", authRoutes)
 app.get("/", (req, res) => {
   res.send("root");
 });
+
+// test auth middleware (jwt)
+app.get("/profile", authenticate, (req, res) => {
+  res.json({
+    "user": req.user
+  })
+})
 
 
 app.listen(PORT, () => {
