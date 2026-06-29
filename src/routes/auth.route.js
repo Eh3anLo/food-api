b23170c8c29eef7a39142e922d63c196afff69d5
Ehsan <ehsunlotfy@gmail.com>
2026-06-29 13:11:41 +0330

    fix: fix Register & Login Request method to POST
    Resolves #31

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/auth.controller")
-const validate = require('../validators/validator')
-const { registerSchema, loginSchema } = require('../validators/schemas/auth.schema')
+const { registerUser, loginUser } = require("../controllers/auth.controller");
+const validate = require("../validators/validator");
+const {
+  registerSchema,
+  loginSchema,
+} = require("../validators/schemas/auth.schema");
 
-router.get("/register",   validate(registerSchema), registerUser)
-router.get("/login", validate(loginSchema), loginUser)
+router.post("/register", validate(registerSchema), registerUser);
+router.post("/login", validate(loginSchema), loginUser);
 
-module.exports = router
+module.exports = router;
